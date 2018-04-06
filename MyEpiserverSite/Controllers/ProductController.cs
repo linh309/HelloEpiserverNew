65a3459b4163c583efc627a90c88d23e9b30244a
linh.nguyen5 <linh.nguyen5@CL-LINHNGUYEN5>
2018-04-06 16:49:59 +0700

    add new PageData and Controller

---
+++ MyEpiserverSite/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Web.Mvc;
+using MyEpiserverSite.Models.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEpiserverSite.Controllers
+{
+    public class ProductController : PageController<ProductPage>
+    {
+        // GET: Product
+        public ActionResult Index(ProductPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
---
+++ MyEpiserverSite/Models/Pages/ProductPage.cs
@@ -0,0 +1,16 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEpiserverSite.Models.Pages
+{
+    [ContentType]
+    public class ProductPage: PageData
+    {
+        public virtual string Title { get; set; }
+        public virtual string Desc { get; set; }
+    }
+}
