79955b1efdc68c194d00a58e128a058d089d186f
linh.nguyen5 <linh.nguyen5@CL-LINHNGUYEN5>
2018-04-09 18:05:36 +0700

    add controller for imagefile

---
+++ MyEpiserverSite/Controllers/ImageFileController.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Web.Mvc;
+using MyEpiserverSite.Models.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEpiserverSite.Controllers
+{
+    public class ImageFileController : PartialContentController<ImageFile>
+    {
+
+        // GET: ImageFile
+        public override ActionResult Index(ImageFile currentContent)
+        {
+
+
+            return View();
+        }
+    }
+}
